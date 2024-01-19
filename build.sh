20865fa6a40d5ae258bac37a67416c529d24cdb0
ankit1219 <102915512+ankit1219@users.noreply.github.com>
2024-01-19 22:43:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+# set -o errexit
 
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 6 -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda install -c conda-forge libta-lib
+pip install TA-Lib
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install gunicorn==20.1.0
+export PATH=“$PATH:/opt/render/miniconda3/lib/python3.10/site-packages”
